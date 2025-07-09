40771cecf9f7d60d73301e2b8a54c53407c88dc1
Manoj <manojkumar944140@gmail.com>
2025-07-09 23:07:03 +0530

    Remove payment folder from Git tracking

--- payment/src/main/java/com/upi/payment/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.upi.payment.repository;
-
-import com.upi.payment.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
